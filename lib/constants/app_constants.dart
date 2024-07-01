99fc6596339cd1c295cd84db28b862936245a2a6
Chandan-M-N <chandan7c77@gmail.com>
2024-07-02 01:40:41 +0530

    initial commit

--- lib/constants/app_constants.dart
+++ lib/constants/app_constants.dart
@@ -1,7 +1,7 @@
 abstract class ThingsboardAppConstants {
-  static const thingsBoardApiEndpoint = 'http://192.168.1.35:8080';
+  static const thingsBoardApiEndpoint = 'http://<your_device_ip>:8080';  //add your device Ip address where the thingsboard server is running.
   static const thingsboardOAuth2CallbackUrlScheme = 'org.thingsboard.app.auth';
 
   /// Not for production (only for debugging)
-  static const thingsboardOAuth2AppSecret = 'RWFZallXbWRXbnJhY2pyS0hFNlFaR1QycGluZUp2TW52b3BYNEFYZjdlYlRRekwwUTEwV3EyVFRZUzhKTWxuRA==';
+  static const thingsboardOAuth2AppSecret = ''; //add oauth2 code
 }
